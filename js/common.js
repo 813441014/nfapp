26e773592cc021a26cc8334400cae63fd3376b41
chuying <chuying@1hok.cn>
2019-11-11 17:19:30 +0800

    Signed-off-by: chuying <chuying@1hok.cn>

---
+++ js/common.js
@@ -0,0 +1,14 @@
+
+//底部导航点击
+function onclicks(obj,num){
+    // $("#bottom li:eq(0)").find("img").attr("src","")
+    $("#bottom li:eq(0)").find("img").attr("src","")
+}
+
+
+//自适应布局
+function getRem(){
+    var html = document.getElementsByTagName("html")[0];
+    var oWidth = document.body.clientWidth || document.documentElement.clientWidth;
+    html.style.fontSize = oWidth/10 + "px";
+}
