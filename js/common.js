256573f726f52321315ee748dbdc937d332625cc
chuying <chuying@1hok.cn>
2019-11-18 13:57:52 +0800

    Signed-off-by: chuying <chuying@1hok.cn>

--- js/common.js
+++ js/common.js
@@ -1,4 +1,9 @@
 
+$(function(){
+    $(".header .goBack").click(function(){
+        history.back(-1);
+    })
+})
 //底部导航点击
 function onclicks(obj,num){
     // $("#bottom li:eq(0)").find("img").attr("src","")
@@ -10,6 +15,8 @@ function onclicks(obj,num){
 function getRem(){
     var html = document.getElementsByTagName("html")[0];
     var oWidth = document.body.clientWidth || document.documentElement.clientWidth;
-    html.style.fontSize = oWidth/10 + "px";
+    // html.style.fontSize = oWidth/10 + "px";
+    // $("html").css("font-size",oWidth/10 +"px!important");
+    html.setAttribute('style', 'font-size:'+oWidth/10 +'px!important');
     $("body").css("display","block")
 }
